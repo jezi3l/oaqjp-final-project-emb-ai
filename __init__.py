eb17c4281cf25dc88ec9bb674f73ea66738109a6
jezi3l <82981383+jezi3l@users.noreply.github.com>
2025-07-20 15:30:08 -0500

    init file added

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+EmotionDetection Flask Application
+A web application for analyzing emotions in text using IBM Watson NLU.
+"""
+
+from emotion_detection import emotion_detector
+
+__version__ = "1.0.0"
+__author__ = "Jeziel A."
+__description__ = "Flask web application for emotion detection"
